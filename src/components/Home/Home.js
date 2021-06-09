4c3167487b3d0b93cbcd5d74efbf64990791266d
jr-ushio <jr.14.ushio@gmail.com>
2021-06-09 18:02:39 -0500

    nueva funcionalidad de paginación

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,22 @@ import PropTypes from 'prop-types';
 import styles from './Home.module.scss';
 import {listar, eliminar} from "../../@services/usuario";
 import {useHistory, useLocation} from "react-router-dom";
+import Pagination from "../Pagination";
 
 const Home = () => {
   const history = useHistory();
   const location = useLocation();
   const [usuarios, setUsuarios] = useState([])
+
+  const [currentPage, setCurrentPage] = useState(2);
+  const [pageSize] = useState(5);
+  const [totalResults, setTotalResults] = useState(1);
+
+  // Pagination
+  const paginate = page => {
+    setCurrentPage(page);
+    listarUsuarios(page)
+  };
 
   const goEdit = (id, nombres, apellidos, usuario) => {
     history.push({
@@ -20,11 +31,12 @@ const Home = () => {
       },
     });
   }
-  const listarUsuarios = () => {
-    listar().then(resp => {
+  const listarUsuarios = (pagina = 1) => {
+    listar(pagina).then(resp => {
       console.log('resp ', resp.data);
       if (resp.data.codigo === 200) {
         setUsuarios(resp.data.data.data)
+        setTotalResults(resp.data.data.total)
       }
     })
   }
@@ -76,6 +88,12 @@ const Home = () => {
             ))}
             </tbody>
           </table>
+          <Pagination
+            totalResults={50} // falta modificar para muchos resultados
+            currentPage={currentPage}
+            pageSize={pageSize}
+            paginate={paginate}
+          />
           <div>
           </div>
         </div>
